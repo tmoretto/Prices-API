31e338137a2611cf31128530efe23cc39f9691f8
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-25 14:25:22 -0300

    Adicionando configuração de cors

--- src/main/java/br/com/prices/prices/api/ProdutoController.java
+++ src/main/java/br/com/prices/prices/api/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<Produto>> obterTodos() {
 		List<Produto> precosProdutos = repository.findAll(); 
