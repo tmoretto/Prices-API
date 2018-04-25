d620de9d56c88997df9690b6834256d512637f35
Tiago Moretto <moretto.tiago@gmail.com>
2018-04-25 13:02:44 -0300

    Inicializando projeto

---
+++ src/test/java/br/com/prices/prices/PricesApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.prices.prices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PricesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
